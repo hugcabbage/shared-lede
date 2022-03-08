7b1987b41acb42280854180983b59c7c50517a83
hugcabbage <xueliu24@163.com>
2022-03-08 15:22:51 +0800

    优化脚本

--- 1clone.sh
+++ 1clone.sh
@@ -2,12 +2,12 @@
 # 下载源码
 git clone https://github.com/coolsnowwolf/lede.git
 mv ./lede/* ./
+
 # 下载自定义插件
-cd package/lean
-rm -rf luci-theme-argon
+rm -rf package/lean/luci-theme-argon
+cd package
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
 git clone https://github.com/jerrykuku/luci-app-argon-config.git
-cd ..
 git clone https://github.com/kenzok8/small.git
 git clone https://github.com/kenzok8/openwrt-packages.git
 git clone https://github.com/Ausaci/luci-app-nat6-helper.git
