13101b222989f5af14aa247048e0b50942c7ab6d
hugcabbage <xueliu24@163.com>
2022-03-14 22:00:51 +0800

    换依赖源

--- preset-models/1clone.sh
+++ preset-models/1clone.sh
@@ -8,6 +8,6 @@ rm -rf package/lean/luci-theme-argon
 cd package
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
 git clone https://github.com/jerrykuku/luci-app-argon-config.git
-git clone https://github.com/kenzok8/small.git
 git clone https://github.com/kenzok8/openwrt-packages.git
 git clone https://github.com/Ausaci/luci-app-nat6-helper.git
+git clone -b packages --single-branch https://github.com/xiaorouji/openwrt-passwall.git
