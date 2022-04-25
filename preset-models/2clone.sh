f03cdcb8378474b684974a41f128b8710ac24652
huya1121 <huya1121@users.noreply.github.com>
2022-04-25 23:13:47 +0800

    Update 2clone.sh

--- preset-models/2clone.sh
+++ preset-models/2clone.sh
@@ -15,3 +15,4 @@ svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash
 git clone -b packages --single-branch https://github.com/xiaorouji/openwrt-passwall.git
 git clone -b luci --single-branch https://github.com/xiaorouji/openwrt-passwall.git luci-app-passwall
 git clone https://github.com/xiaorouji/openwrt-passwall2.git
+git clone https://github.com/messense/aliyundrive-webdav.git
