2182550ac02d63be357b53e8d26c1f6ece329a03
hugcabbage <xueliu24@163.com>
2022-06-01 20:55:49 +0800

    优化

--- preset-models/target_select.py
+++ preset-models/target_select.py
@@ -1,45 +1,51 @@
 #!/usr/bin/python3
 
+import os
 import sys
 import shutil
 import fileinput
 
-main_targets = ["mi-router-4a-gigabit","mi-router-cr6608","redmi-router-ac2100","phicomm_k2p"]
-main_numbers = ["1","2","3","4"]
-sub_targets = ["mi-router-cr6606","mi-router-cr6609","mi-router-3g","mi-router-4","mi-router-3-pro","mi-router-ac2100","mi-router-3g-v2"]
-sub_numbers = ["2-1","2-2","2-3","2-4","2-5","3-1","1-1"]
+os.chdir("preset-models")
+
+main_targets = ["mi-router-4a-gigabit", "mi-router-cr6608", "redmi-router-ac2100", "phicomm_k2p"]
+main_numbers = ["1", "2", "3", "4"]
+sub_targets = ["mi-router-cr6606", "mi-router-cr6609", "mi-router-3g", "mi-router-4", "mi-router-3-pro", "mi-router-ac2100", "mi-router-3g-v2"]
+sub_numbers = ["2-1", "2-2", "2-3", "2-4", "2-5", "3-1", "1-1"]
 
 def get_number():
-    i = 1
     for target in main_targets:
         if target == sys.argv[1]:
-            return i
+            i = main_targets.index(target)
+            return main_numbers[i]
             break
-        i += 1
     else:
-        j = 0
         for target in sub_targets:
             if target == sys.argv[1]:
+                j = sub_targets.index(target)
                 return sub_numbers[j]
                 break
-            j += 1
 
-def copy_files(old_num,new_num,old_str,new_str):
-    config_files = [".config","clone.sh","modify.sh"]
-    for config_file in config_files:
-        shutil.copyfile(old_num + config_file,new_num + config_file)
-    for line in fileinput.input(new_num + config_files[0], inplace=1):
-        line = line.replace(old_str,new_str)
-        print(line, end = "")
+def copy_files(old_pre, new_pre, old_str="", new_str=""):
+    co_files = [".config", "clone.sh", "modify.sh"]
+    for co_file in co_files:
+        shutil.copyfile(old_pre + co_file, new_pre + co_file)
+    if old_str != "" and new_str != "":
+        for line in fileinput.input(new_pre + co_files[0], inplace=1):
+            line = line.replace(old_str, new_str)
+            print(line, end = "")
     return
 
-temp_num = get_number()
+# 获取序号
+num = get_number()
 
-print(temp_num)
+print(num)
 
 #复制子机型配置文件
-if isinstance(temp_num,str):
-    pre_num = temp_num.split("-")[0]
-    m = main_numbers.index(pre_num)
-    n = sub_numbers.index(temp_num)
-    copy_files(pre_num,temp_num,main_targets[m],sub_targets[n])
+if "-" in num:
+    num_part1 = num.split("-")[0]
+    m = main_numbers.index(num_part1)
+    n = sub_numbers.index(num)
+    copy_files(num_part1, "temp.", main_targets[m], sub_targets[n])
+else:
+    copy_files(num, "temp.")
+os.rename("temp..config", "temp.config")
