cf88b99bb4b3d3830c72298d2f18ffeb46d5045d
hug cabbage <xueliu24@163.com>
2022-10-15 22:03:46 +0800

    部分文件重名及错误修复

---
+++ preset-models/headers.py
@@ -0,0 +1,14 @@
+Headers = {
+    'xiaomi-4a-gigabit': ('1', 'ramips', 'mt7621', 'xiaomi_mi-router-4a-gigabit'),
+    'xiaomi-3g-v2': ('1', 'ramips', 'mt7621', 'xiaomi_mi-router-3g-v2'),
+    'xiaomi-cr6608': ('2', 'ramips', 'mt7621', 'xiaomi_mi-router-cr6608'),
+    'xiaomi-cr6606': ('2', 'ramips', 'mt7621', 'xiaomi_mi-router-cr6606'),
+    'xiaomi-cr6609': ('2', 'ramips', 'mt7621', 'xiaomi_mi-router-cr6609'),
+    'xiaomi-3g': ('2', 'ramips', 'mt7621', 'xiaomi_mi-router-3g'),
+    'xiaomi-4': ('2', 'ramips', 'mt7621', 'xiaomi_mi-router-4'),
+    'xiaomi-3-pro': ('2', 'ramips', 'mt7621', 'xiaomi_mi-router-3-pro'),
+    'redmi-ac2100': ('3', 'ramips', 'mt7621', 'xiaomi_redmi-router-ac2100'),
+    'xiaomi-ac2100': ('3', 'ramips', 'mt7621', 'xiaomi_mi-router-ac2100'),
+    'phicomm-k2p': ('4', 'ramips', 'mt7621', 'phicomm_k2p'),
+    'redmi-ax6s': ('5', 'ramips', 'mt7621', 'xiaomi_redmi-router-ax6s')
+}
