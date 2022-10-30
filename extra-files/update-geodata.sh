e18ad7ca6f3a093e7e4bdd0df4d0ed02c619a03c
hugcabbage <77980779+hugcabbage@users.noreply.github.com>
2022-10-30 15:40:30 +0800

    额外文件: 路径修复

--- extra-files/update-geodata.sh
+++ extra-files/update-geodata.sh
@@ -1,16 +1,12 @@
 #!/bin/sh
 
-GEODIR="package/passwall-dependencies/v2ray-geodata"
+GEODIR='package/supply-packages/pw-dependencies/v2ray-geodata'
 
-wget -O v2ray-rules-dat.latest https://api.github.com/repos/Loyalsoldier/v2ray-rules-dat/releases/latest
+LATEST_VER=$(curl -fs https://api.github.com/repos/Loyalsoldier/v2ray-rules-dat/releases/latest | jq -r .'tag_name')
 
-LATEST_VER=$(jq -r .tag_name v2ray-rules-dat.latest)
+GEOIP_HASH=$(curl -fsL https://github.com/Loyalsoldier/v2ray-rules-dat/releases/download/$LATEST_VER/geoip.dat.sha256sum | awk '{print $1}')
 
-wget https://github.com/Loyalsoldier/v2ray-rules-dat/releases/download/$LATEST_VER/geoip.dat.sha256sum
-wget https://github.com/Loyalsoldier/v2ray-rules-dat/releases/download/$LATEST_VER/geosite.dat.sha256sum
-
-GEOIP_HASH=$(sed -n '1p' geoip.dat.sha256sum | cut -d ' ' -f 1)
-GEOSITE_HASH=$(sed -n '1p' geosite.dat.sha256sum | cut -d ' ' -f 1)
+GEOSITE_HASH=$(curl -fsL https://github.com/Loyalsoldier/v2ray-rules-dat/releases/download/$LATEST_VER/geosite.dat.sha256sum | awk '{print $1}')
 
 sed -i "/GEOIP_VER:=/cGEOIP_VER:=$LATEST_VER" $GEODIR/Makefile
 sed -i "/GEOSITE_VER:=/cGEOSITE_VER:=$LATEST_VER" $GEODIR/Makefile
@@ -22,5 +18,3 @@ sed -i "/FILE:=\$(GEOSITE_FILE)/a\  HASH:=$GEOSITE_HASH" $GEODIR/Makefile
 sed -i 's|v2fly/geoip|Loyalsoldier/v2ray-rules-dat|g' $GEODIR/Makefile
 sed -i 's|v2fly/domain-list-community|Loyalsoldier/v2ray-rules-dat|g' $GEODIR/Makefile
 sed -i 's|dlc.dat|geosite.dat|g' $GEODIR/Makefile
-
-rm -f v2ray-rules-dat.latest geoip.dat.sha256sum geosite.dat.sha256sum
