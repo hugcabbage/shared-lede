7415bd0313c5eaa30b56ce22316cc77d6baa8968
hugcabbage <77980779+hugcabbage@users.noreply.github.com>
2022-11-09 19:47:23 +0800

    添加immortalwrt编译源

---
+++ preset-immortalwrt/1.clone.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# 下载源码
+git clone --single-branch -b openwrt-21.02 https://github.com/immortalwrt/immortalwrt.git
+mv ./immortalwrt/* ./
+
+# 下载插件
+SUPPLY_DIR=supply-packages
+echo "src-link supply $PWD/$SUPPLY_DIR" >> feeds.conf.default
+mkdir $SUPPLY_DIR && cd $SUPPLY_DIR
+git clone --depth 1 https://github.com/jerrykuku/luci-theme-argon.git
+git clone --depth 1 https://github.com/jerrykuku/luci-app-argon-config.git
+git clone --depth 1 https://github.com/yichya/luci-app-xray.git
+git clone --depth 1 -b packages https://github.com/xiaorouji/openwrt-passwall.git pw-dependencies
+svn export https://github.com/xiaorouji/openwrt-passwall/branches/luci/luci-app-passwall
