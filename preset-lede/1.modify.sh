2ab6e958e3e9198bf5634c2c62d7d3eab460e0a0
hugcabbage <77980779+hugcabbage@users.noreply.github.com>
2023-03-17 20:41:50 +0800

    preset-lede：修复cr660x

--- preset-lede/1.modify.sh
+++ preset-lede/1.modify.sh
@@ -12,9 +12,7 @@ sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_genera
 #sed -i 's/Xiaomi Mi Router 3G v2/Xiaomi 3G v2/g' target/linux/ramips/dts/mt7621_xiaomi_mi-router-3g-v2.dts
 #sed -i 's/Xiaomi Redmi Router AC2100/Redmi AC2100/g' target/linux/ramips/dts/mt7621_xiaomi_redmi-router-ac2100.dts
 #sed -i 's/Xiaomi Mi Router AC2100/Xiaomi AC2100/g' target/linux/ramips/dts/mt7621_xiaomi_mi-router-ac2100.dts
-#sed -i 's/Xiaomi Mi Router CR6606/Xiaomi CR6606/g' target/linux/ramips/dts/mt7621_xiaomi_mi-router-cr6606.dts
-#sed -i 's/Xiaomi Mi Router CR6608/Xiaomi CR6608/g' target/linux/ramips/dts/mt7621_xiaomi_mi-router-cr6608.dts
-#sed -i 's/Xiaomi Mi Router CR6609/Xiaomi CR6609/g' target/linux/ramips/dts/mt7621_xiaomi_mi-router-cr6609.dts
+#sed -i 's/Xiaomi Mi Router CR660x/Xiaomi CR660x/g' target/linux/ramips/dts/mt7621_xiaomi_mi-router-cr6606.dts
 
 #复制smartdns配置
 rm -rf feeds/packages/net/smartdns
