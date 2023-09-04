0e08a5caeeab8b26ecb47596a8429c1a43386091
hugcabbage <77980779+hugcabbage@users.noreply.github.com>
2023-09-04 05:20:28 +0000

    preset-lienol-openwrt：删除多余行

--- preset-lienol-openwrt/1.modify.sh
+++ preset-lienol-openwrt/1.modify.sh
@@ -28,9 +28,6 @@ rm -rf package/feeds/packages/xray-core
 mkdir -p files/etc/uci-defaults
 cp $DEPLOYDIR/uci-scripts/* files/etc/uci-defaults/
 
-#复制内核5.10版本CPU超频补丁
-#cp extra-files/322-mt7621-fix-cpu-clk-add-clkdev.patch target/linux/ramips/patches-5.10/
-
 #设置WIFI
 #sed -i 's/OpenWrt/coolxiaomi/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 #sed -i 's/wireless.default_radio${devidx}.encryption=none/wireless.default_radio${devidx}.encryption=psk-mixed/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
@@ -38,9 +35,6 @@ cp $DEPLOYDIR/uci-scripts/* files/etc/uci-defaults/
 
 #修改登录密码为coolxiaomi
 #sed -i '/root/croot:$1$CBd7u73H$LvSDVXLBrzpk4JfuuN.Lv1:18676:0:99999:7:::' package/base-files/files/etc/shadow
-
-#切换ramips内核到5.15
-#sed -i '/KERNEL_PATCHVER/cKERNEL_PATCHVER:=5.15' target/linux/ramips/Makefile
 
 #删除一些zzz中的命令
 ZZZ_PATH='package/default-settings/files/zzz-default-settings'
