c9495798662211395031bd54f2e56320e2de3ef8
hugcabbage <77980779+hugcabbage@users.noreply.github.com>
2023-10-18 19:59:56 +0800

    templet: update

--- templet/produce.py
+++ templet/produce.py
@@ -3,19 +3,20 @@ import os
 import sys
 import glob
 import shutil
-import subprocess
 
 
 import toml
 
 
 sys.path.append(os.path.dirname(os.path.dirname(__file__)) + '/extra-files')
-from tools import simplify, routine_cmd
-from tools.crypt import crypt_root
+from tools.crypt_text import crypt_str
+from tools.routine_cmd import gen_dot_config
+from tools.process_text import simplify_config
 
 
-# 获取编号
 def get_serial(directory: str) -> str:
+    """Get the serial number"""
+
     if (total := len(glob.glob(f'{directory}/*clone.sh'))) == 0:
         number = '1'
     else:
@@ -28,29 +29,31 @@ def get_serial(directory: str) -> str:
     return number
 
 
-# 按链接提取应用名
 def extract_app_name(links: list) -> list:
+    """Extract app name from links"""
+
     apps = []
     for link in links:
         apps += [f'* {link.split("@")[0].split("/")[-1].rstrip(".git")}\n']
     return apps
 
 
-# 按链接生成git clone命令
 def produce_git_command(link: str, isbase=False) -> str:
+    """Produce git command by link"""
+
     path = ''
     link = link.strip().rstrip('@')
-    # 0个@
+    # no @
     if '@' not in (url := link):
         command = f'git clone --depth 1 {url}'
     else:
         segment = link.split('@')
         url = segment[0]
-        # 1个@
+        # one @
         if link.count('@') == 1:
             branch = segment[1]
             command = f'git clone --depth 1 -b {branch} {url}'
-        # 2个@
+        # two @
         else:
             if segment[1] == '':
                 path = segment[2]
@@ -68,13 +71,14 @@ def produce_git_command(link: str, isbase=False) -> str:
     return command
 
 
-# 按链接生成svn命令
 def produce_svn_command(link: str) -> str:
+    """Produce svn command by link"""
+
     link = link.strip().rstrip('@')
-    # 0个@
+    # no @
     if '@' not in (url := link):
         command = f'svn export {url}'
-    # 1个@
+    # one @
     else:
         segment = link.split('@')
         url = segment[0]
@@ -83,14 +87,15 @@ def produce_svn_command(link: str) -> str:
     return command
 
 
-# 生成模板化配置文件
 def produce_conf(data: dict, prefix: str) -> bool:
-    # 生成clone.sh
+    """Generate template-based configuration files"""
+
+    # Generate clone.sh
     basecommands = produce_git_command(ba := data['base'], True) + '\n'
     appcommands = []
     try:
         if data['git_app'] == '':
-            raise KeyError('值不能为空')
+            raise KeyError('The value cannot be empty')
         for link in data['git_app']:
             appcommands += [f'{produce_git_command(link)}\n']
         ga = True
@@ -98,7 +103,7 @@ def produce_conf(data: dict, prefix: str) -> bool:
         ga = False
     try:
         if data['svn_app'] == '':
-            raise KeyError('值不能为空')
+            raise KeyError('The value cannot be empty')
         for link in data['svn_app']:
             appcommands += [f'{produce_svn_command(link)}\n']
         sa = True
@@ -108,18 +113,18 @@ def produce_conf(data: dict, prefix: str) -> bool:
     if ga or sa:
         try:
             if data['app_path'] == '':
-                raise KeyError('值不能为空')
+                raise KeyError('The value cannot be empty')
             if (path := data["app_path"]).startswith('package/') or path == 'package':
                 pass
             else:
-                print('app_path未在package目录内')
+                print('app_path not in package folder')
                 sys.exit()
             dla = [
                 '\n# download app codes\n',
                 f'mkdir -p {path} && cd {path}\n'
-                ]
+            ]
         except KeyError:
-            print('app源已存在，但缺少app_path')
+            print('app source has existed, but the lack app_path')
             sys.exit()
     text1 = [
         '#!/bin/sh\n',
@@ -127,36 +132,42 @@ def produce_conf(data: dict, prefix: str) -> bool:
         basecommands
     ] + dla
     text1 += appcommands
-    with open(prefix + '.clone.sh', 'w') as f:
+    with open(prefix + '.clone.sh', 'w', encoding='utf-8') as f:
         f.writelines(text1)
-    # 生成modify.sh
+
+    # Generate modify.sh
     text2 = [
         '#!/bin/sh\n',
         '\n# modify login IP\n'
     ]
     try:
         if data['login_ip'] == '':
-            raise KeyError('值不能为空')
-        text2.append(f'sed -i \'s/192.168.1.1/{data["login_ip"]}/g\' package/base-files/files/bin/config_generate\n')
+            raise KeyError('The value cannot be empty')
+        text2.append(
+            f'sed -i \'s/192.168.1.1/{data["login_ip"]}/g\' package/base-files/files/bin/config_generate\n')
         li = True
     except KeyError:
-        text2.append('# sed -i \'s/192.168.1.1/192.168.51.1/g\' package/base-files/files/bin/config_generate\n')
+        text2.append(
+            '# sed -i \'s/192.168.1.1/192.168.51.1/g\' package/base-files/files/bin/config_generate\n')
         li = False
     text2.append('\n# modify login password\n')
     n = text2.index('\n# modify login password\n')
     try:
         if data['login_pwd'] == '':
-            raise KeyError('值不能为空')
-        text2.append(f"sed -i '/root/c{crypt_root(data['login_pwd'])}' package/base-files/files/etc/shadow\n")
-        text2[n] = text2[n].replace('password', f'password to {data["login_pwd"]}')
+            raise KeyError('The value cannot be empty')
+        text2.append(
+            f"sed -i '/root/c{crypt_str(data['login_pwd'])}' package/base-files/files/etc/shadow\n")
+        text2[n] = text2[n].replace(
+            'password', f'password to {data["login_pwd"]}')
         lp = True
     except KeyError:
-        text2.append(f"# sed -i '/root/c{crypt_root('888888')}' package/base-files/files/etc/shadow\n")
+        text2.append(
+            f"# sed -i '/root/c{crypt_str('888888')}' package/base-files/files/etc/shadow\n")
         text2[n] = text2[n].replace('password', 'password to 888888')
         lp = False
     if 'openwrt/openwrt' in ba:
         text2 += [
-            '\n# turn on wireless\n',
+            '\n# copy uci-defaults script(s)\n',
             'mkdir -p files/etc/uci-defaults\n',
             'cp preset-openwrt/uci-scripts/17_wireless files/etc/uci-defaults/\n',
             '\n# modify default package(s)\n',
@@ -165,13 +176,14 @@ def produce_conf(data: dict, prefix: str) -> bool:
         offi = True
     else:
         offi = False
-    with open(prefix + '.modify.sh', 'w') as f:
+    with open(prefix + '.modify.sh', 'w', encoding='utf-8') as f:
         f.writelines(text2)
-    # 生成.config
+
+    # Generate .config
     text3 = [
         f'CONFIG_TARGET_{(t1 := data["board"])}=y\n',
         f'CONFIG_TARGET_{t1}_{(t2 := data["subtarget"])}=y\n',
-        f'CONFIG_TARGET_{t1}_{t2}_DEVICE_{(t3 := data["device"])}=y\n'
+        f'CONFIG_TARGET_{t1}_{t2}_DEVICE_{data["device"]}=y\n'
     ]
     if offi:
         extra_t = [
@@ -181,13 +193,14 @@ def produce_conf(data: dict, prefix: str) -> bool:
             'CONFIG_LUCI_LANG_zh_Hans=y\n'
         ]
         text3 += extra_t
-    with open(prefix + '.config', 'w') as f:
+    with open(prefix + '.config', 'w', encoding='utf-8') as f:
         f.writelines(text3)
-    # 生成release.md
+
+    # Generate release.md
     text4 = [
         f'## {data["base_name"]} for {data["device_name"]}\n',
-        f'\nversion：{data["base_version"]}\n',
-        '\nlogin info：\n'
+        f'\nversion: {data["base_version"]}\n',
+        '\nlogin info: \n'
     ]
     if li:
         text4.insert(3, f'* IP {data["login_ip"]}\n')
@@ -198,18 +211,19 @@ def produce_conf(data: dict, prefix: str) -> bool:
     else:
         text4.insert(4, '* Password default\n')
     if ga or sa:
-        text4.append('\napplications：\n')
+        text4.append('\napplications: \n')
         if ga:
             text4 += extract_app_name(data['git_app'])
         if sa:
             text4 += extract_app_name(data['svn_app'])
-    with open(prefix + '.release.md', 'w') as f:
+    with open(prefix + '.release.md', 'w', encoding='utf-8') as f:
         f.writelines(text4)
     return extra_t
 
 
 def main():
-    destdir = (rp1 := os.getenv('REPO_PATH').rstrip('/')) + '/' + (dd1 := os.getenv('DEPLOYDIR').rstrip('/'))
+    destdir = (rp1 := os.getenv('REPO_PATH').rstrip('/')) + \
+        '/' + (dd1 := os.getenv('DEPLOY_DIR').rstrip('/'))
     ba1 = f'{destdir}/backups'
     wf1 = f'{rp1}/.github/workflows'
     if os.getenv('DELETE_ALL') == 'true':
@@ -242,9 +256,10 @@ def main():
     initfile = rp1 + '/' + os.getenv('INITFILE')
     with open(initfile) as f:
         extra_t = produce_conf(tl1 := toml.load(f), serial)
-    routine_cmd.gen_dot_config(serial + '.clone.sh', serial + '.config')
-    simplify.simplify_config(serial + '.config', remain_text=extra_t)
-    # 移动文件到目标文件夹，准备commit
+    gen_dot_config(serial + '.clone.sh', serial + '.config')
+    simplify_config(serial + '.config', remain_text=extra_t)
+
+    # Move file to the target folder, prepare for commit
     if os.getenv('OVERWRITE_LAST') == 'true':
         for item in glob.glob(f'{wf1}/{dd1}-{serial}*'):
             os.remove(item)
@@ -253,10 +268,15 @@ def main():
             shutil.move(item, f'{ba1}/{item}')
         else:
             shutil.move(item, f'{destdir}/{item}')
-    shutil.copyfile(f'{rp1}/templet/build.yml', by1 := f"{wf1}/{dd1}-{serial}-{tl1['device_name'].replace(' ', '-')}.yml")
-    subprocess.run(f"sed -i 's/name: xxxxxx/name: {dd1} {tl1['device_name']}/' {by1}", shell=True)
-    subprocess.run(f"sed -i 's/SERIAL_NU: xxxxxx/SERIAL_NU: {serial}/' {by1}", shell=True)
-    subprocess.run(f"sed -i 's/DEPLOYDIR: xxxxxx/DEPLOYDIR: {dd1}/' {by1}", shell=True)
+
+    with open(f'{rp1}/templet/build.yml', encoding='utf-8') as f1, \
+            open(f'{wf1}/{dd1}-{serial}-{tl1["device_name"].replace(" ", "-")}.yml', 'w', encoding='utf-8') as f2:
+        text = f1.read()
+        text = text.replace(
+            'name: xxxxxx', f'name: {dd1} {tl1["device_name"]}')
+        text = text.replace('SERIAL_NU: xxxxxx', f'SERIAL_NU: {serial}')
+        text = text.replace('DEPLOY_DIR: xxxxxx', f'DEPLOY_DIR: {dd1}')
+        f2.write(text)
 
 
 if __name__ == '__main__':
